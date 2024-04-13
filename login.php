358ca313fa8c2ce2ccaf351d06d12ccef06159ad
abhijitbomble <81073210+abhijitbomble@users.noreply.github.com>
2024-04-13 10:12:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php 
-   session_start();
+   require("logindata.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,35 +13,6 @@
 <body>
       <div class="container">
         <div class="box form-box">
-            <?php 
-             
-              include("config.php");
-              if(isset($_POST['submit'])){
-                $email = mysqli_real_escape_string($con,$_POST['email']);
-                $password = mysqli_real_escape_string($con,$_POST['password']);
-
-                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
-                $row = mysqli_fetch_assoc($result);
-
-                if(is_array($row) && !empty($row)){
-                    $_SESSION['valid'] = $row['Email'];
-                    $_SESSION['username'] = $row['Username'];
-                    $_SESSION['age'] = $row['Age'];
-                    $_SESSION['id'] = $row['Id'];
-                }else{
-                    echo "<div class='message'>
-                      <p>Wrong Username or Password</p>
-                       </div> <br>";
-                   echo "<a href='login.php'><button class='btn'>Go Back</button>";
-         
-                }
-                if(isset($_SESSION['valid'])){
-                    header("Location: home.php");
-                }
-              }else{
-
-            
-            ?>
             <header>Login</header>
             <form action="" method="post">
                 <div class="field input">
