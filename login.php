a5a7f1291637e8ee2241d779f14ab3ff83035cbb
abhijitbomble <81073210+abhijitbomble@users.noreply.github.com>
2024-04-18 14:02:13 +0530

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
 
                 <div class="field">
                     
-                    <input type="submit" class="btn" href="home.php" name="submit" value="Login" required>
+                    <input type="button" onclick="window.location.href = "home.php" class="btn" name="submit" value="Login" required>
                 </div>
                 <div class="links">
                     Don't have account? <a href="register.php">Sign Up Now</a>
